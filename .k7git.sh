7778ef48dfa1d712d203dd040c7abb6b7ec230b3
Ubuntu <ubuntu@ubuntu.lan>
2020-07-16 21:59:37 +0000

    Git updates

--- .k7git.sh
+++ .k7git.sh
@@ -10,3 +10,24 @@ alias gco='git checkout '
 alias gcm='git checkout master'
 
 alias gpr='git pull --rebase '
+
+export K7_TRUE_ORIGIN=origin
+gprom () {
+    if [ $# -lt 1 ]; then
+        CMD="git pull --rebase $K7_TRUE_ORIGIN master"
+    else
+        CMD="git pull --rebase $1 master"
+    fi;
+    echo $CMD
+    $CMD
+}
+
+gfo () {
+    if [ $# -lt 1 ]; then
+        CMD="git fetch $K7_TRUE_ORIGIN"
+    else
+        CMD="git fetch $1"
+    fi;
+    echo $CMD
+    $CMD
+}
